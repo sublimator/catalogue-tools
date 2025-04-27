0837afc55807f49dfcac91e5e1d18be030b7cc8c
Nicholas Dudfield <ndudfield@gmail.com>
2025-04-27 14:52:47 +0700

    fix: fix conan profile show

--- scripts/conan.sh
+++ scripts/conan.sh
@@ -4,17 +4,17 @@
 #   set -e to fail on any command error
 set -e
 
-ROOT_DIR=`git rev-parse --show-toplevel || pwd` # in case we are not in a git repo, such as the Dockerfile
+ROOT_DIR=$(git rev-parse --show-toplevel || pwd) # in case we are not in a git repo, such as the Dockerfile
 cd $ROOT_DIR
 echo "Root dir: $ROOT_DIR"
 
 # Check if conan is v2 or exist the script
 if conan --version | grep -q "Conan version 2"; then
-    echo "Conan v2 detected"
-    conan --version
+	echo "Conan v2 detected"
+	conan --version
 else
-    echo "Conan v1 detected. Exiting. Try 'source scripts/setup-catenv.sh'"
-    exit 1
+	echo "Conan v1 detected. Exiting. Try 'source scripts/setup-catenv.sh'"
+	exit 1
 fi
 
 BUILD_TYPE=${BUILD_TYPE:-Debug}
@@ -28,40 +28,35 @@ conan_profiles_home=$(conan config home)/profiles
 mkdir -p "$conan_profiles_home"
 # check if there's a detected profile
 if [ ! -f "$conan_profiles_home/detected" ]; then
-    # if not, create a detected profile
-    conan profile detect --name detected
+	# if not, create a detected profile
+	conan profile detect --name detected
 fi
 
 if [ -n "$CONFIGURE_GCC_13_PROFILE" ]; then
-    # Create a detected profile
-        conan profile detect --name detected
+	# Create a custom profile file
+	mkdir -p "$(conan config home)/profiles"
+	cat >$(conan config home)/profiles/default <<EOF
+include(detected)
 
-        # Create a custom profile file
-        mkdir -p "$(conan config home)/profiles"
-        cat >$(conan config home)/profiles/default <<EOF
-    include(detected)
-
-    [settings]
-    compiler.cppstd=20
-    compiler.libcxx=libstdc++
-    [conf]
-    tools.build:compiler_executables={"c": "/usr/bin/gcc-13", "cpp": "/usr/bin/g++-13"}
+[settings]
+compiler.cppstd=20
+compiler.libcxx=libstdc++
+[conf]
+tools.build:compiler_executables={"c": "/usr/bin/gcc-13", "cpp": "/usr/bin/g++-13"}
 EOF
 else
-  # If a default profile does not exist, create one
-  if [ ! -f "$conan_profiles_home/default" ]; then
-      cat >$(conan config home)/profiles/default <<EOF
-      include(detected)
+	# If a default profile does not exist, create one
+	if [ ! -f "$conan_profiles_home/default" ]; then
+		cat >$(conan config home)/profiles/default <<EOF
+include(detected)
 
-      [settings]
-      compiler.cppstd=20
+[settings]
+compiler.cppstd=20
 EOF
-  fi
+	fi
 fi
 
-
-
 conan install $ROOT_DIR \
-      --build=missing \
-      --output-folder="$BUILD_DIR" \
-      --settings build_type="$BUILD_TYPE"
+	--build=missing \
+	--output-folder="$BUILD_DIR" \
+	--settings build_type="$BUILD_TYPE"
