03cc7bad93bedff39583fc85ccfa639f5814f4f9
Nicholas Dudfield <ndudfield@gmail.com>
2025-08-31 12:27:32 +0700

    docs: document reason for single line file

--- cmake/Protobuf.cmake
+++ cmake/Protobuf.cmake
@@ -1,33 +1,7 @@
+# All this file does is this, because we weren't aware that all that was
+# required to fix protoc / libprotobuf mismatches was to use the
+# `tool_requires` section in conanfile.txt
+# Previously, we had some custom cmake logic to try and find the correct
+# protoc. We'll just leave this file in place in case there's a
+# need for it later.
 find_package(Protobuf REQUIRED)
-
-## Force CMake to use Conan's protoc by setting the PROTOC variable
-#if(protobuf_INCLUDE_DIR)
-#  # Derive the bin directory from include directory
-#  get_filename_component(PROTOBUF_ROOT "${protobuf_INCLUDE_DIR}" DIRECTORY)
-#  set(CONAN_PROTOC_PATH "${PROTOBUF_ROOT}/bin/protoc")
-#
-#  if(EXISTS "${CONAN_PROTOC_PATH}")
-#    message(STATUS "Found Conan protoc at: ${CONAN_PROTOC_PATH}")
-#
-#    # Force CMake's FindProtobuf to use our protoc
-#    set(Protobuf_PROTOC_EXECUTABLE
-#        "${CONAN_PROTOC_PATH}"
-#        CACHE FILEPATH "The protoc compiler" FORCE)
-#    set(PROTOBUF_PROTOC_EXECUTABLE
-#        "${CONAN_PROTOC_PATH}"
-#        CACHE FILEPATH "The protoc compiler" FORCE)
-#
-#    # Verify version
-#    execute_process(
-#      COMMAND ${Protobuf_PROTOC_EXECUTABLE} --version
-#      OUTPUT_VARIABLE PROTOC_VERSION
-#      OUTPUT_STRIP_TRAILING_WHITESPACE)
-#    message(STATUS "Protoc version: ${PROTOC_VERSION}")
-#  else()
-#    message(FATAL_ERROR "Conan protoc not found at: ${CONAN_PROTOC_PATH}")
-#  endif()
-#else()
-#  message(
-#    WARNING "protobuf_INCLUDE_DIR not set. Will use system protoc if available."
-#  )
-#endif()
