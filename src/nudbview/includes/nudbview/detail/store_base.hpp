bf890acf66843acb6e0d98e5a2a03d7ca4cc4d12
Nicholas Dudfield <ndudfield@gmail.com>
2025-11-09 08:31:08 +0700

    feat: add nudbview as fork of NuDB for read-only views
    Vendor NuDB as nudbview fork to enable:
    - Read-only views with subslice .key files
    - .tree file format (append-only structural sharing log)
    - Offline rotation/defragmentation tools
    
    Setup includes:
    - Header-only library in src/nudbview/
    - Minimal CMake integration with Boost dependencies
    - Basic smoke test in tests/nudbview/
    - Boost Software License 1.0 preserved from original NuDB
    
    This fork will be extended to support .dat file subsetting and
    .tree index files for efficient ledger history management.

---
+++ src/nudbview/includes/nudbview/detail/store_base.hpp
@@ -0,0 +1,48 @@
+//
+// Copyright (c) 2019 Miguel Portilla (miguelportilla64 at gmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef NUDB_DETAIL_STORE_BASE_HPP
+#define NUDB_DETAIL_STORE_BASE_HPP
+
+namespace nudbview {
+
+class context;
+
+namespace detail {
+
+class store_base
+{
+public:
+    virtual ~store_base() = default;
+
+protected:
+    friend class nudbview::context;
+
+    virtual void flush() = 0;
+
+private:
+#if ! NUDB_DOXYGEN
+    friend class test::context_test;
+#endif
+
+    enum class state
+    {
+        waiting,
+        flushing,
+        intermediate,
+        none
+    };
+
+    store_base* next_ = nullptr;
+    store_base* prev_ = nullptr;
+    state state_ = state::none;
+};
+
+} // detail
+} // nudb
+
+#endif
---
+++ src/nudbview/includes/nudbview/nudb.hpp
@@ -0,0 +1,27 @@
+//
+// Copyright (c) 2015-2016 Vinnie Falco (vinnie dot falco at gmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef NUDB_HPP
+#define NUDB_HPP
+
+#include <nudbview/concepts.hpp>
+#include <nudbview/create.hpp>
+#include <nudbview/error.hpp>
+#include <nudbview/file.hpp>
+#include <nudbview/posix_file.hpp>
+#include <nudbview/progress.hpp>
+#include <nudbview/recover.hpp>
+#include <nudbview/rekey.hpp>
+#include <nudbview/store.hpp>
+#include <nudbview/type_traits.hpp>
+#include <nudbview/verify.hpp>
+#include <nudbview/version.hpp>
+#include <nudbview/visit.hpp>
+#include <nudbview/win32_file.hpp>
+#include <nudbview/xxhasher.hpp>
+
+#endif
