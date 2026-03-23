287c5aa302c252fcb4203a25f7f2e0e2613f83ad
Nicholas Dudfield <ndudfield@gmail.com>
2026-03-23 19:51:56 +0700

    fix: add braces to avoid dangling-else warning from gtest macros

--- tests/shamap/binary-trie-gtest.cpp
+++ tests/shamap/binary-trie-gtest.cpp
@@ -342,7 +342,9 @@ TEST(BinaryTrie, ByteLevelStructure)
     for (int i = 0; i < 16; i++)
     {
         if (i != 10)
+        {
             EXPECT_EQ(get_branch_type(header, i), BranchType::empty);
+        }
     }
 
     // Leaf: 32-byte key
